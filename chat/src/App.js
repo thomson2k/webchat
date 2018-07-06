082042bbebd012b057557bb6ce2a9a76acc9d6f0
wirycapybara <w.t.tomasz@gmail.com>
2018-07-06 15:44:56 +0200

    added conv

--- chat/src/App.js
+++ chat/src/App.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Conversation (props) {
+    return (
+      <div className="conversation">
+        {props.list.map(statement => (
+          <div className="statement">
+            {statement}
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+
 class App extends Component {
   render() {
+    const list = [
+      'for',
+       'example'
+    ]
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Conversation list={list} />
       </div>
     );
   }
